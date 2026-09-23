7509aaa700305a01229d5f0c13b8066a9087cbb3
Erika.Lopez <erika.lopez.sanz@opendeusto.es>
2026-09-23 09:14:11 +0200

    he creado una ventana

--- EjemploGit/src/Ventana.java
+++ EjemploGit/src/Ventana.java
@@ -1,6 +1,22 @@
+import javax.swing.JFrame;
 
-public class Ventana {
+public class Ventana extends JFrame {
 
+	private static final long serialVersionUID = 1L;
+	
+	public Ventana() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(320, 240);
+		
+		setTitle("Prueba de Git");
+		
+		setVisible(true);
+	}
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
